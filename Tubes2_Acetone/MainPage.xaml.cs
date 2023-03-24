93cdee5479ca648093ec2cc98e9889f2bac2579e
christodharma <13521009@std.stei.itb.ac.id>
2023-03-24 15:58:23 +0700

    macos filepicker

--- Tubes2_Acetone/MainPage.xaml.cs
+++ Tubes2_Acetone/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+using System.ComponentModel;
 
 namespace Tubes2_Acetone;
 
@@ -41,6 +43,7 @@ public partial class MainPage : ContentPage
         {
             FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
         {
+            { DevicePlatform.macOS, new[] { "txt" } },
             { DevicePlatform.iOS, new[] { "public.plain-text" } },
             { DevicePlatform.Android, new[] { "text/plain" } },
             { DevicePlatform.UWP, new[] { ".txt" } },
@@ -50,9 +53,47 @@ public partial class MainPage : ContentPage
         if (result != null)
         {
             string FilePath = result.FullPath;
-            // Do something with the selected file
             ((MainViewModel)BindingContext).FilePath = result.FullPath;
+            // Do something with the selected file
+            // Read the text file contents
+            string fileContents = File.ReadAllText(FilePath);
+
+            // Split the contents into lines
+            string[] lines = fileContents.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Iterate over the lines and characters to add Image controls to the Grid
+            for (int row = 0; row < lines.Length; row++)
+            {
+                for (int col = 0; col < lines[row].Length; col++)
+                {
+                    // Create an Image control for the current character
+                    Image image = new Image();
+                    if (lines[row][col] == 'X')
+                    {
+                        // Set the image source for walls
+                        image.Source = "wall";
+                    }
+                    else if (lines[row][col] == 'T')
+                    {
+                        image.Source = "treasure";
+                        image.Scale = 0.1;
+                    } else if (lines[row][col] == 'R')
+                    {
+                        image.Source = "visited";
+                    }
+                    /*
+                    else
+                    {
+                        // Set the image source for other objects
+                        image.Source = "object";
+                    }
+                    */
+                    // Add the Image control to the Grid
+                    Grid.SetRow(image, row);
+                    Grid.SetColumn(image, col);
+                    MapGrid.Children.Add(image);
+                }
+            }
         }
     }
 }
-
