668b5d7f282d9b29367abf2c41baf6d91b60b9a4
christodharma <13521009@std.stei.itb.ac.id>
2023-03-24 11:27:09 +0700

    Choosing txt file

--- Tubes2_Acetone/MainPage.xaml.cs
+++ Tubes2_Acetone/MainPage.xaml.cs
@@ -1,12 +1,58 @@
-﻿namespace Tubes2_Acetone;
+﻿using System.ComponentModel;
+
+namespace Tubes2_Acetone;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
+        BindingContext = new MainViewModel();
 	}
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _filePath;
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                if (_filePath != value)
+                {
+                    _filePath = value;
+                    OnPropertyChanged(nameof(FilePath));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    [Obsolete]
+    private async void OnChooseFileClicked(object sender, EventArgs e)
+    {
+        var result = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.iOS, new[] { "public.plain-text" } },
+            { DevicePlatform.Android, new[] { "text/plain" } },
+            { DevicePlatform.UWP, new[] { ".txt" } },
+        })
+        });
+
+        if (result != null)
+        {
+            string FilePath = result.FullPath;
+            // Do something with the selected file
+            ((MainViewModel)BindingContext).FilePath = result.FullPath;
+        }
+    }
 }
 
